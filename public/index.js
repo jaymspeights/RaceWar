e88fb513c4779e16b20861c4d908c9e666744db5
jaymspeights <jaymspeights@gmail.com>
2017-07-09 00:59:02 -0500

    game v1

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ $(function(){
   var b1 = document.getElementById('multi-btn');
   b1.addEventListener('click', function(){
     $.get('/multi', function (data) {
-      $('.result').html(data);
+      $('#game').html(data);
     });
   });
   var b2 = document.getElementById('single-btn');
   b2.addEventListener('click', function(){
     $.get('/single', function (data) {
-      $('.result').html(data);
+      $('#game').html(data);
     });
   });
 });
